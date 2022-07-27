81f59796d3e38b79dbe782b1d5086e28513dfda6
Yanet Palma <yan.designer@live.com.mx>
2022-07-27 13:49:29 -0500

    Cambios 27 jun 22

---
+++ java-script/app.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById ('form')
+
+formulario.addEventListener ('submit', function(e){
+
+    e.preventDefault();
+    let mail= document.getElementById('email-form').value
+    console.log (mail)
+})
